aac286e1b94a63f5188a6959cbea1c415c369173
Redshirt256 <43748817+Redshirt256@users.noreply.github.com>
2020-06-04 04:11:56 +0200

    Kopierad från privat repository
    Hemsidan har ingen content men layout är klar

---
+++ script.js
@@ -0,0 +1,11 @@
+function openleft() {
+    document.getElementById("leftnav").style.visibility = "visible";
+    document.getElementById("burgerbutton").style.visibility = "hidden";
+    /* document.getElementById ("parent").style.margin = "200px"; */
+  }
+
+  function closeleft() {
+    document.getElementById("leftnav").style.visibility= "hidden";
+    document.getElementById("burgerbutton").style.visibility = "visible";
+    /* document.getElementById("parent").style.margin = "0px"; */
+  }
